005c9b9c67c429f885ffd89582deba9c169d6193
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2022-07-16 10:31:28 +1200

    Moved to gnu-efi headers

--- Main.c
+++ Main.c
@@ -1,24 +1,4 @@
-#define IN
-#define EFIAPI
-#define EFI_SUCCESS 0
-
-typedef unsigned short CHAR16;
-typedef unsigned long long EFI_STATUS;
-typedef void* EFI_HANDLE;
-
-struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
-typedef EFI_STATUS(EFIAPI* EFI_TEXT_STRING)(IN struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* This, IN CHAR16* String);
-
-typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
-    void* a;
-    EFI_TEXT_STRING OutputString;
-} EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
-
-typedef struct {
-    char a[52];
-    EFI_HANDLE ConsoleOutHandle;
-    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* ConOut;
-} EFI_SYSTEM_TABLE;
+#include "gnu-efi/inc/efi.h"
 
 EFI_STATUS EFIAPI EfiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable) {
     SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello World!\n");
