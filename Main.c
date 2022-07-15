bf122c0058980211a9d883f1f9fcd1fbd3c77b47
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2022-07-16 11:53:15 +1200

    Added detection of pixel format

--- Main.c
+++ Main.c
@@ -1,8 +1,25 @@
 #include <stddef.h>
 #include "gnu-efi/inc/efi.h"
 
-void PutPixel(EFI_GRAPHICS_OUTPUT_PROTOCOL* gop, size_t x, size_t y, uint32_t color) {
-    *((uint32_t*)(gop->Mode->FrameBufferBase + 4 * gop->Mode->Info->PixelsPerScanLine * y + 4 * x)) = color;
+typedef struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} Color;
+
+void PutPixel(EFI_GRAPHICS_OUTPUT_PROTOCOL* gop, EFI_GRAPHICS_OUTPUT_MODE_INFORMATION* info, size_t x, size_t y, Color color) {
+    uint8_t* pixel = (uint8_t*)(gop->Mode->FrameBufferBase + 4 * gop->Mode->Info->PixelsPerScanLine * y + 4 * x);
+    if (info->PixelFormat == PixelRedGreenBlueReserved8BitPerColor) {
+        pixel[0] = color.r;
+        pixel[1] = color.g;
+        pixel[2] = color.b;
+    } else if (info->PixelFormat == PixelBlueGreenRedReserved8BitPerColor) {
+        pixel[0] = color.b;
+        pixel[1] = color.g;
+        pixel[2] = color.r;
+    } else {
+        // do nothing
+    }
 }
 
 EFI_STATUS EFIAPI EfiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
@@ -30,9 +47,15 @@ EFI_STATUS EFIAPI EfiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
     (void)nativeMode;
     (void)numModes;
 
+    if (info->PixelFormat != PixelRedGreenBlueReserved8BitPerColor &&
+        info->PixelFormat != PixelBlueGreenRedReserved8BitPerColor) {
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Unsupported GOP pixel format\n");
+        return EFI_UNSUPPORTED;
+    }
+
     for (size_t y = 0; y < gop->Mode->Info->VerticalResolution; y++) {
         for (size_t x = 0; x < gop->Mode->Info->HorizontalResolution; x++) {
-            PutPixel(gop, x, y, 0x00FF0000);
+            PutPixel(gop, info, x, y, (Color){ .r = 255, .g = 0, .b = 0 });
         }
     }
 
